abffc8899077278524c55c32bbff15a3a35cec89
mvaleck <minivaleck@hotmail.com>
2025-03-17 16:47:48 -0300

    descrição das receitas

--- code/script2.js
+++ code/script2.js
@@ -39,11 +39,15 @@ document.addEventListener('DOMContentLoaded', function () { //faz o JavaScript e
 
     // Descrições reais para cada receita, 
     const descricoes = {
-        "Panqueca": "Ingredientes: Farinha, leite, ovos... <br> Modo de preparo: Misture tudo...",
-        "Geleia de Frutas": "Ingredientes: Morango, açúcar... <br> Modo de preparo: Cozinhe as frutas...",
-        "Pão de Queijo": "Ingredientes: Polvilho, queijo... <br> Modo de preparo: Misture e asse...",
-        "Macarrão à Bolonhesa": "Ingredientes: Macarrão, carne moída... <br> Modo de preparo: Cozinhe o molho...",
-        "Risoto de Camarão": "Ingredientes: Arroz arbóreo, camarão... <br> Modo de preparo: Refogue e adicione o caldo...",
+    "Panqueca": "Ingredientes: 1 xícara de farinha de trigo, 1/2 de leite, 1 ovo, 1 colher de sopa de açúcar, 1 colher de chá de fermento em pó... <br><br> Modo de preparo: Misture todos os ingredientes até formar uma massa homogênea. Aqueça uma frigideira e despeje a massa formando círculos pequenos. Cozinhe por 2 minutos de cada lado ou até dourar.",
+    "Geleia de Frutas": "Ingredientes: 2 xícaras de frutas (morango, amora, etc.), 1 xícara de açúcar, suco de 1 limão. <br><br> Modo de preparo: Cozinhe as frutas com o açúcar e o limão até obter uma consistência de geleia. Deixe esfriar antes de servir.",
+    "Pão de Queijo": "Ingredientes: 2 xícaras de polvilho doce, 1 xícara de queijo ralado, 1/2 xícara de leite, 1/4 xícara de óleo, 1 ovo. <br><br> Modo de preparo: Misture todos os ingredientes até formar uma massa. Modele bolinhas e asse a 180°C por 20 minutos ou até dourar.",
+    "Macarrão à Bolonhesa": "Ingredientes: 250g de macarrão, 300g de carne moída, 1 lata de molho de tomate, 1 cebola picada, 2 dentes de alho picados. <br><br> Modo de preparo: Cozinhe o macarrão. Refogue a carne moída com a cebola e o alho, adicione o molho de tomate e cozinhe por 10 minutos. Sirva o molho sobre o macarrão.",
+    "Risoto de Camarão": "Ingredientes: 1 xícara de arroz para risoto, 300g de camarão, 1 cebola, 1/2 xícara de vinho branco, 4 xícaras de caldo de legumes.<br><br> Modo de preparo: Refogue a cebola, adicione o arroz e o vinho. Vá acrescentando o caldo aos poucos até o arroz ficar al dente. Finalize com os camarões cozidos.",
+    "Carne de Panela": "Ingredientes: 500g de carne de panela, 2 batatas, 2 cenouras, 1 cebola, 2 dentes de alho, 1 folha de louro. <br><br>Modo de prepraro: Cozinhe a carne com a cebola, alho e folha de louro. Adicione as batatas e cenouras e cozinhe até ficarem macias.",
+	"Lasanha": "Ingredientes: 500g de massa para lasanha, 300g de carne moída, 1 molho de tomate, 300g de queijo mussarela, 1 cebola, 1 dente de alho.<br><br> Modo de prepraro: Cozinhe a massa. Refogue a carne com cebola e alho, adicione o molho de tomate. Monte a lasanha alternando camadas de massa, molho e queijo. Asse a 180°C por 30 minutos.",
+	"Feijoada": "Ingredientes:500g de feijão preto, 300g de carne seca, 200g de linguiça, 1 cebola, 2 dentes de alho. <br><br> Modo de prepraro: Cozinhe o feijão com as carnes e a cebola. Refogue o alho e misture ao feijão. Cozinhe até todos os ingredientes ficarem macios.",
+	"Filé ao Molho Madeira": "Ingredientes: 500g de filé mignon, 1 xícara de molho madeira, 1 colher de sopa de manteiga, 1 cebola. <br><br> Modo de prepraro: Grelhe o filé mignon e reserve. Refogue a cebola na manteiga, adicione o molho madeira e cozinhe por alguns minutos. Sirva o molho sobre o filé.",
 
 
     };
